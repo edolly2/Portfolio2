33908ddbb45a740fd4d7721f6570a1c64974d0ba
edolly2 <edolly2@protonmail.com>
2021-02-10 06:30:23 -0600

    "External JS"

---
+++ port.js
@@ -0,0 +1,16 @@
+function openNav() {
+    document.getElementById('myNav').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '320px';
+}
+
+function closeNav() {
+    document.getElementById('myNav').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+}
+
+window.addEventListener('orientationchange', function(event) {
+    console.log(
+        'the orientation of the device is now' +
+        event.target.screen.orientation.angle
+    );
+});
