7cde6a00e4895fdfd0092f00df46d4fab6eb09ab
edolly2 <edolly2@protonmail.com>
2021-03-03 03:47:13 -0600

    "jQuery"

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(() => {
+    $('.form-btn').on('click', () => {
+        $('#myModal').show();
+        $('.modal-content').slideDown(1000);
+    });
+
+    $('#submit').on('click', (event) => {
+        $('#myModal').hide();
+    });
+    $('#submit').on('click', () => {
+        alert('form sent');
+    });
+});
